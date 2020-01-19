62636520e5d754bcfe1dda1b40e3b5e3a36f21a8
wuuyi <wuuyi123@gmail.com>
2020-01-19 09:37:04 +0700

    remove viet.js

--- src/viet.js
+++
@@ -1,9 +0,0 @@
-window.onload = function()
-{
-    var code = document.getElementById('code');
-    var run = document.getElementById('run');
-
-    run.onclick = function() {
-        (new VM).interpret(code.value);
-    };
-};
